40165510dbb129e4bd6f580aa737cb08d141ff23
Steve Kinney <hello@stevekinney.net>
2017-01-28 19:12:19 -0700

    Import configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from 'firebase';
+import config from '../env.js';
 import moment from 'moment';
-
-const config = {
-  apiKey: "AIzaSyAiFDobN7hkPfbqRLKn4E5hvDKVrZDW6o4",
-  authDomain: "spening-tracker.firebaseapp.com",
-  databaseURL: "https://spening-tracker.firebaseio.com",
-  storageBucket: "spening-tracker.appspot.com",
-  messagingSenderId: "973100448978"
-};
 
 export default firebase.initializeApp(config);
 export const provider = new firebase.auth.GoogleAuthProvider();
